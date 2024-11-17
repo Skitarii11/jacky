0d218d37e91e6cc93ea497f37a4daeea910bbaa5
Javkhlan <jawhaa_1107@yahoo.com>
2024-11-17 23:51:56 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({setShowLogin}) => {
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-            <a href='https://brownslud.com/' onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""} target="_blank">about us</a>
+            <a href='' onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""} target="_blank">about us</a>
             <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
         <div className="navbar-right">
