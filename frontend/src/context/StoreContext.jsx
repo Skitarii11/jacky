2cd45fc455148bd4062a5405f15848afd306351e
Skitarii11 <jawhaa_1107@yahoo.com>
2025-03-12 17:33:39 +0800

    ndasjbd

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -51,6 +51,7 @@ const StoreContextProvider = (props) => {
      useEffect(()=>{
         async function loadData(){
             await fetchFoodList();
+            console.log(setFoodList)
             if(localStorage.getItem("token")){
                 setToken(localStorage.getItem("token"))
                 await loadCartdata(localStorage.getItem("token"))
