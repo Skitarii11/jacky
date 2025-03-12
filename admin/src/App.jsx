a0894f96dda18544acbd2e2918524b7782091656
Skitarii11 <jawhaa_1107@yahoo.com>
2025-03-12 16:57:36 +0800

    ndasjbd

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "http://localhost:10000"
+  const url =  process.env.REACT_APP_API_ENDPOINT
 
   return (
     <div>
